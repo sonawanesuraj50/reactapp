6766502a79ed9ebdfe2f6f6c92b1fe71d1dfed2d
sonawanesuraj50 <59729108+sonawanesuraj50@users.noreply.github.com>
2021-01-10 00:43:39 +0530

    react
    react

---
+++ src/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+//import { makeStyles } from '@material-ui/core/styles';
+
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//       flexGrow: 1,
+//     },
+//     menuButton: {
+//       marginRight: theme.spacing(2),
+//     },
+//     title: {
+//       flexGrow: 1,
+//     },
+//   }));
+
+function Home() {
+    //const classes = useStyles();
+
+  return (
+    <>
+<button type="button" class="btn btn-primary">Primary</button>
+    </>
+  );
+}
+
+export default Home;
